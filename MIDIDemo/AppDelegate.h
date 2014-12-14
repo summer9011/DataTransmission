9e48f91f294a641a74fe53f20ee8ff606a998df0
summer9011 <zhao_li_bo@163.com>
2014-12-14 22:03:23 +0800

    Initial Commit

---
+++ MIDIDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MIDIDemo
+//
+//  Created by 赵立波 on 14/12/14.
+//  Copyright (c) 2014年 赵立波. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MIDIDemo/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  MIDIDemo
+//
+//  Created by 赵立波 on 14/12/14.
+//  Copyright (c) 2014年 赵立波. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
